4b095fe1d638b412fd7bdbf9eb9cc3fdc38095ff
Coding_With_Tewuhbo <126251177+tewmih@users.noreply.github.com>
2025-02-11 15:47:24 +0300

    Time: 0 ms (100%), Space: 18 MB (82.22%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,9 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort()
-        counter=0
         h=0
-        print(citations)
         for i in range(len(citations)):
             if len(citations)-i<=citations[i]:
                 h=max(h,len(citations)-i)
